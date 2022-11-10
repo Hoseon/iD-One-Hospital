c0724f6bce29499a42196c4a64f60662c7c0cfb4
hoseon <chs1025@gmail.com>
2022-11-10 10:45:30 +0900

    Initial Commit

---
+++ iD-One Hospital/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iD-One Hospital
+//
+//  Created by HoSeon Chu on 2022/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
